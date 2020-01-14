27ce797f3b2586f8e473e1df87be1c3b267eefda
DanielleInkster <danielle.inkster@gmail.com>
2020-01-14 23:35:59 +0000

    missing comma

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,5 @@
 class Player{
-  constructor(ship1 = new Ship(3), ship2 = new Ship(4), ship3 = new Ship(5) board = new Board) {
+  constructor(ship1 = new Ship(3), ship2 = new Ship(4), ship3 = new Ship(5), board = new Board) {
     this.ship1 = ship1
     this.ship2 = ship2
     this.ship3 = ship3
