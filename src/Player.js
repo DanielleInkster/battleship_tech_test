1252fecd223d8601ab8c3a03b15da74b7ab5206e
DanielleInkster <danielle.inkster@gmail.com>
2020-01-14 23:35:29 +0000

    Dependency injection for board/player

--- src/Player.js
+++ src/Player.js
@@ -1,11 +1,11 @@
 class Player{
-  constructor(ship1 = new Ship(3), ship2 = new Ship(4), ship3 = new Ship(5)) {
+  constructor(ship1 = new Ship(3), ship2 = new Ship(4), ship3 = new Ship(5) board = new Board) {
     this.ship1 = ship1
     this.ship2 = ship2
     this.ship3 = ship3
     this.points = 12
     this.turn = false
-    this.board = new Board
+    this.board = board
     this.shipPositions = []
   }
 }
