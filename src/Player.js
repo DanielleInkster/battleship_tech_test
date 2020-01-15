ea02fa82d8d7a1f986b1324a305fc20c12f6c3c1
DanielleInkster <danielle.inkster@gmail.com>
2020-01-15 18:32:20 +0000

    Fix for positioning conflict

--- src/Player.js
+++ src/Player.js
@@ -9,9 +9,9 @@ class Player{
   }
 
   positionShip(shipNum, [...coordinates]) { 
+    this.fleet[shipNum].checkLength(...coordinates),
+    this.board.checkIfOccupied(...coordinates),
     this.fleet[shipNum].setPosition()
-    this.fleet[shipNum].checkLength(...coordinates)
-    this.board.checkIfOccupied(...coordinates)
     coordinates.forEach( item => { this.fleet[shipNum].coordinates.push(item) && this.board.occupiedCoordinates.push(item)}
     )}
 }
